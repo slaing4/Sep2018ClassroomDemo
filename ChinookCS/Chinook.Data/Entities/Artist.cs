0bd775416145243cf2b17fa2e4b4714a04e4b017
slaing4 <35244635+slaing4@users.noreply.github.com>
2018-09-10 19:35:00 -0600

    Added NuGet And Artist Entity

--- ChinookCS/Chinook.Data/Entities/Artist.cs
+++ ChinookCS/Chinook.Data/Entities/Artist.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Chinook.Data
+namespace Chinook.Data.Entities
 {
-    public class Class1
+    public class Artist
     {
     }
 }
