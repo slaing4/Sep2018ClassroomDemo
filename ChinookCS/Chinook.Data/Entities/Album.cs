47693cac099bc1a96eb11014fde6fed9a6669f93
slaing4 <35244635+slaing4@users.noreply.github.com>
2018-09-10 20:01:51 -0600

    Albums and Artist Entites
    Idk if they are done or not but they are worked on

---
+++ ChinookCS/Chinook.Data/Entities/Album.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Addtitional Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+namespace Chinook.Data.Entities
+{
+    [Table("Albums")]
+    public class Album
+    {
+        [Key]
+        public int AlbumId { get; set; }
+        [Required(ErrorMessage ="Title is required")]
+        [StringLength(160, ErrorMessage ="Title is limited to 160 characters")]
+        public string Title { get; set; }
+        [Required(ErrorMessage ="Artist ID is a required")]
+        public int ArtistId { get; set; }
+        public int Releaseyear { get; set; }
+        [StringLength(160, ErrorMessage = "Release label is limited to 160 characters")]
+        public string ReleaseLabel { get; set; }
+
+    }
+}
--- ChinookCS/Chinook.Data/Entities/Artist.cs
+++ ChinookCS/Chinook.Data/Entities/Artist.cs
@@ -4,9 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+#region Additonal Namespaces
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+#endregion
+
+
 namespace Chinook.Data.Entities
 {
+    [Table("Artists")]
     public class Artist
     {
+        [Key, Column(Order = 1)]
+        public int ArtistId { get; set; }
+        [Required(ErrorMessage ="Name is a required feild")]
+        [StringLength(120, ErrorMessage = "Maximum Length 120 charatcers")]
+        public string Name { get; set; }
     }
 }
