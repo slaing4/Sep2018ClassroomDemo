a90d7f7c0a5622e662262536234bf73953131871
slaing4 <35244635+slaing4@users.noreply.github.com>
2018-09-12 19:58:19 -0600

    Created Artist Controller
    Artist Controller:
    List Artist - lists all artist
    Artist_Find - Gets artist using artistid parameter

---
+++ ChinookCS/ChinookSystem/BLL/ArtistController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using Chinook.Data.Entities;
+using ChinookSystem.DAL;
+#endregion
+
+
+namespace ChinookSystem.BLL
+{
+    public class ArtistController
+    {
+        //needs to be public because it is accessed by things outside of this library
+
+        public List<Artist> Artist_List()
+        {
+            //lists the artists?
+            using(var context = new ChinookContext())
+            {
+                return context.Artists.ToList();
+            }
+        }
+
+        public Artist Artist_Find(int artistid)
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Artists.Find(artistid);
+            }
+        }
+
+
+
+    }
+}
