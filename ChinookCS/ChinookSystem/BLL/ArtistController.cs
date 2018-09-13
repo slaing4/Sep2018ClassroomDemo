2aa39a0e5e6244ffb383fa1fee859bdc272ff737
slaing4 <35244635+slaing4@users.noreply.github.com>
2018-09-12 20:02:33 -0600

    DataObjectMethod in ArtistController
    Not sure why though

--- ChinookCS/ChinookSystem/BLL/ArtistController.cs
+++ ChinookCS/ChinookSystem/BLL/ArtistController.cs
@@ -7,15 +7,17 @@ using System.Threading.Tasks;
 #region Additional Namespaces
 using Chinook.Data.Entities;
 using ChinookSystem.DAL;
+using System.ComponentModel;
 #endregion
 
 
 namespace ChinookSystem.BLL
 {
+    [DataObject]
     public class ArtistController
     {
         //needs to be public because it is accessed by things outside of this library
-
+        [DataObjectMethod(DataObjectMethodType.Select, false)] //IDK WHAT THIS IS THOUGH, 
         public List<Artist> Artist_List()
         {
             //lists the artists?
@@ -25,6 +27,7 @@ namespace ChinookSystem.BLL
             }
         }
 
+        [DataObjectMethod(DataObjectMethodType.Select, false)]//STILL IDK
         public Artist Artist_Find(int artistid)
         {
             using (var context = new ChinookContext())
