cdd681eaa486d340faa30fca50a8ae049301ae6a
slaing4 <35244635+slaing4@users.noreply.github.com>
2018-09-12 20:09:52 -0600

    AlbumController Added
    Basic functions of select all and find by id, did without help but should be in working order

---
+++ ChinookCS/ChinookSystem/BLL/AlbumController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespace
+using Chinook.Data.Entities;
+using ChinookSystem.DAL;
+using System.ComponentModel;
+#endregion
+
+namespace ChinookSystem.BLL
+{
+    [DataObject]
+    public class AlbumController
+    {
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public List<Album> List_Albums()
+        {
+            using (var context = new ChinookContext())
+            {
+                return context.Albums.ToList();
+            }
+        }
+
+        [DataObjectMethod(DataObjectMethodType.Select, false)]
+        public Album Album_Find(int albumid)
+        {
+            using(var context = new ChinookContext())
+            {
+                return context.Albums.Find(albumid);
+            }
+        }
+
+
+    }
+}
