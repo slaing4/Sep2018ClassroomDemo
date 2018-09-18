6358a9ed5f5a7cdd88d9c2a06990d76a8ad2fcc8
slaing4 <35244635+slaing4@users.noreply.github.com>
2018-09-17 20:01:16 -0600

    added ods and listview
    remove the artist from

--- ChinookCS/WebApp/SamplePages/ListVew.aspx.designer.cs
+++ ChinookCS/WebApp/SamplePages/ListVew.aspx.designer.cs
@@ -13,13 +13,13 @@ namespace WebApp.SamplePages {
     public partial class ListVew {
         
         /// <summary>
-        /// SelectArtistLabel control.
+        /// Label1 control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Label SelectArtistLabel;
+        protected global::System.Web.UI.WebControls.Label Label1;
         
         /// <summary>
         /// ArtistList control.
@@ -40,13 +40,13 @@ namespace WebApp.SamplePages {
         protected global::System.Web.UI.WebControls.LinkButton FetchArtist;
         
         /// <summary>
-        /// AlbumListView control.
+        /// AlbumList control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ListView AlbumListView;
+        protected global::System.Web.UI.WebControls.ListView AlbumList;
         
         /// <summary>
         /// ArtistListODS control.
