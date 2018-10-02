eb58283e64047b73b8a853a2565ba32513696238
slaing4 <35244635+slaing4@users.noreply.github.com>
2018-10-01 19:08:59 -0600

    Putting Linq into ASP.NET
    Created the POCOs (flat data set) folder in the Chinook.Data library
    
    Added PlaylistSummary class to the POCOs folder and made it public
    
    Added Attributes tot he PlaylistSummary POCO by copying them from the LinqDay3 file:
    public string name {get;set;}
    	public int trackcount {get;set;}
    	public decimal cost {get;set;}
    	public double? storage {get;set;}

---
+++ ChinookCS/Chinook.Data/POCOs/PlaylistSumary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chinook.Data.POCOs
+{
+    public class PlaylistSumary
+    {
+        public string name { get; set; }
+        public int trackcount { get; set; }
+        public decimal cost { get; set; }
+        public double? storage { get; set; } // nullable
+
+    }
+}
